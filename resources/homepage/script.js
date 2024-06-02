f7b324d706ba70a1fdb70541223547ec2bb70a33
Whiteboard <sir.whiteboard@gmail.com>
2024-06-01 23:18:29 -0400

    Homepage, Flood, About added/updated

--- resources/homepage/script.js
+++ resources/homepage/script.js
@@ -3,14 +3,23 @@ document.addEventListener('DOMContentLoaded', () => {
     const outputDiv = document.getElementById('output');
 
     const commands = {
-        help: 'Available commands: help, directory, hello, date, clear',
+        //Real commands
+        help: 'Available commands: help, directory, about, hello, date, clear',
         hello: 'Hello, world!',
         date: new Date().toString(),
-        directory: '<a href="/piracy.html">/piracy</a>\n<a href="/ming.html">/ming</a>',
-        clear: ''
+        directory: '<a href="/piracy.html">/piracy</a>\n<a href="/flood.html">/flood</a>\n<a href="/ming.html">/ming</a>',
+        piracy: '',
+        flood: '',
+        ming: '',
+        about: '',
+        clear: '',
+
+        //Fun commands
+        octagon: '<a href="https://www.youtube.com/watch?v=ddWJatRxfz8" target="_blank"rel="noopener noreferrer">🛑</a>',
+        zork: "Oh hey there's a Zork command here"
     };
 
-    const welcomeMessage = `You find yourself somewhere, yet nowhere at the same time.\nType 'directory' to view all accessible pages.\nType 'help' to view available commands.\n`;
+    const welcomeMessage = `You find yourself somewhere, yet nowhere at the same time.\nType 'directory' to view all accessible pages.\nType 'help' to view available commands.\nType 'about' to jump to the about page.\nPretend the background is high quality for the time being.\n`;
 
     // Display welcome message
     outputDiv.innerHTML += `${welcomeMessage}\n`;
@@ -18,10 +27,15 @@ document.addEventListener('DOMContentLoaded', () => {
     commandInput.addEventListener('keydown', (event) => {
         if (event.key === 'Enter') {
             const command = commandInput.value.trim().toLowerCase();
+            // Goofy ass yandere dev programming
             if (command in commands) {
                 if (command === 'clear') {
                     outputDiv.innerHTML = '';
                     outputDiv.innerHTML += `${welcomeMessage}\n`;
+                } else if (command === 'piracy' || command === 'flood' || command === 'ming' || command === 'about') {
+                    window.location.href = "/" + command + ".html"
+                } else if (command === 'zork') {
+                    window.location = "https://archive.org/details/msdos_Zork_I_-_The_Great_Underground_Empire_1980"
                 } else {
                     outputDiv.innerHTML += `$ ${command}\n${commands[command]}\n\n`;
                 }
